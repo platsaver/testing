70e39c36418604e7f1699c6db3d5fffea9fe3264
platsaver <kingdomofhellborn123@gmail.com>
2025-03-11 21:31:35 +0700

    Adding form pseudo function

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const emailConfig = {
-    host: "smtp.gmail.com",
-    username: "letsgobrandon1w@gmail.com",
-    password: "dfcffefgacdrzwpd" // Thay bằng App Password thực tế của Gmail
-};
-
-export default emailConfig;
